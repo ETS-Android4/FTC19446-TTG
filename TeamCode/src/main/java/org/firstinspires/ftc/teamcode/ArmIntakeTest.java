c64d64ecc632f8d83fedf1866bc36fd728b1f11a
FrancisMadarangSchool <francis.madarang22@ycdsbk12.ca>
2021-11-17 14:45:22 -0500

    updated ArmIntakeTest.java to test the intake with controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmIntakeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmIntakeTest.java
@@ -4,13 +4,12 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Blinker;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Gyroscope;
 
 @TeleOp
 
 public class ArmIntakeTest extends LinearOpMode {
     private DcMotor LF, RF, LB, RB, Arm, Intake;
-    private Gyroscope imu;
+    private Blinker expansion_Hub_2;
 
     public void runOpMode() {
         LF = hardwareMap.get(DcMotor.class, "motorFrontLeft");
@@ -18,8 +17,21 @@ public class ArmIntakeTest extends LinearOpMode {
         LB = hardwareMap.get(DcMotor.class, "motorBackLeft");
         RB = hardwareMap.get(DcMotor.class, "motorBackRight");
 
-        imu = hardwareMap.get(Gyroscope.class, "imu");
+        Arm = hardwareMap.get(DcMotor.class, "poggy");
 
-        Arm = hardwareMap.get(DcMotor.class, "asdfasdf");
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                Arm.setPower(-1);
+            } else if (gamepad1.b) {
+                Arm.setPower(1);
+            } else {
+                Arm.setPower(0);
+            }
+            telemetry.addData("Arm Power:", Arm.getPower());
+            telemetry.update();
+
+        }
     }
 }
